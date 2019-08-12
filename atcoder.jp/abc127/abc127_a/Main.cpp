18c3acfe126a7c2bc7fddddee4c1acc80e0abb1a
matu684 <g1620535@is.ocha.ac.jp>
2019-08-12 23:49:34 +0900

    [AC] abc127 abc127_a

---
+++ atcoder.jp/abc127/abc127_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int A, B;
+	cin >> A >> B;
+	if(A>=13){
+		cout << B <<endl;
+	}
+	else if(A<=5) {
+		cout << 0 << endl;
+	}
+	else{
+		cout << B/2 <<endl;
+	}
+}
