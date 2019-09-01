c0f666c366160e0c19a135dac6fe851e87bc58ab
matu684 <g1620535@is.ocha.ac.jp>
2019-09-01 22:30:39 +0900

    [AC] abc139 abc139_d

---
+++ atcoder.jp/abc139/abc139_d/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ll n;
+	cin >> n;
+	cout << n*(n-1)/2 << endl;
+}
