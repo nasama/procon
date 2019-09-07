74c2968c02498dc2dc0548050ac9766815772cbd
matu684 <g1620535@is.ocha.ac.jp>
2019-09-07 21:01:19 +0900

    [AC] abc140 abc140_a

---
+++ atcoder.jp/abc140/abc140_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+	int N;
+	cin >> N;
+	cout << N*N*N;
+}
