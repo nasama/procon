b8b05697815bfe7102eaf7d83c3aebb1851b7e54
matu684 <g1620535@is.ocha.ac.jp>
2019-09-28 21:04:57 +0900

    [AC] abc142 abc142_a

---
+++ atcoder.jp/abc142/abc142_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+ 
+ 
+int main() {
+	float N;
+	cin >> N;
+	cout << ((int)N+1)/2/N << endl;
+}
