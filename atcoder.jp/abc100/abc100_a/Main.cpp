b701bed70c8607a2e09e2645756e300faa809445
matu684 <g1620535@is.ocha.ac.jp>
2020-01-08 20:45:28 +0900

    [AC] abc100 abc100_a

---
+++ atcoder.jp/abc100/abc100_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define rep2(i,n) for (int i = 1; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int A, B;
+    cin >> A >> B;
+    if(A>8||B>8) cout << ":(" << endl;
+    else cout << "Yay!" << endl;
+    return 0;
+}
