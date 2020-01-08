6cc4a4c0368b25efe66331a026848bd633db372f
matu684 <g1620535@is.ocha.ac.jp>
2020-01-08 20:50:28 +0900

    [AC] abc098 abc098_a

---
+++ atcoder.jp/abc098/abc098_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define rep2(i,n) for (int i = 1; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int A, B;
+    cin >> A >> B;
+    cout <<  max(max(A+B, A-B), A*B) << endl;
+    return 0;
+}
