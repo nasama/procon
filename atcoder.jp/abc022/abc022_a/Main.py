e3fefdcba44221822d4244d6a5368af8d5005328
matu684 <g1620535@is.ocha.ac.jp>
2020-03-15 15:35:35 +0900

    [AC] abc022 abc022_a

---
+++ atcoder.jp/abc022/abc022_a/Main.py
@@ -0,0 +1,11 @@
+N, S, T = map(int, input().split())
+W = int(input())
+ans = 0
+if S <= W <= T:
+  ans += 1
+for i in range(2,N+1):
+  a = int(input())
+  W += a
+  if S <= W <= T:
+      ans+=1
+print(ans)
