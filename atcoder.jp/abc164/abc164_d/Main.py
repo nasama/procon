a613121e3a2d26c7e104b304edbc90a228d449c6
matu684 <g1620535@is.ocha.ac.jp>
2020-04-27 16:36:09 +0900

    [AC] abc164 abc164_d

---
+++ atcoder.jp/abc164/abc164_d/Main.py
@@ -0,0 +1,20 @@
+s = input()[::-1]
+
+sum_of_digits = 0
+cnts = [0]*2019
+cnts[0] = 1
+
+d = 1
+
+for c in s:
+    sum_of_digits += int(c) * d
+    sum_of_digits %= 2019
+    d *= 10
+    d %= 2019
+    cnts[sum_of_digits] += 1
+
+ans = 0
+for cnt in cnts:
+    ans += cnt * (cnt - 1) // 2
+
+print(ans)
